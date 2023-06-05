e40a2bf9f0c29e3b0d3e45632b08b447be5f141e
Lakshmi <bandarulakshminarasimham@gmail.com>
2023-06-05 14:57:16 +0800

    added

---
+++ DataMiner-Repo1_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DataMiner-Repo1_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("DataMiner-Repo1_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("10D470A8-2983-4773-A1DE-6DF2EA5839BD")]
+[assembly: AssemblyVersion("1.0.0.0")]
